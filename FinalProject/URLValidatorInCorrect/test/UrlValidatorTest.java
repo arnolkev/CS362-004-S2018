76c36243e65137d9a07510b7b5d16bc1801e00ae
illia-osu <abdullai@oregonstate.edu>
2018-05-19 16:39:55 -0700

    Final Project part B: add a few manual tests.

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,49 +1,41 @@
-
-
 import junit.framework.TestCase;
-
-//You can use this as a skeleton for your 3 different test approach
-//It is an optional to use this file, you can generate your own test file(s) to test the target function!
-// Again, it is up to you to use this file or not!
-
-
-
 
 
 public class UrlValidatorTest extends TestCase {
+    public UrlValidatorTest(String testName) {
+        super(testName);
+    }
 
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
-   }
-
-   
-   
-   public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	 //You can use this function to implement your First Partition testing	   
-
-   }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
-   }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
-
-   }
-   
+    public void testManualTest() {
+        UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        assertTrue(validator.isValid("http://www.google.com"));
+        assertFalse(validator.isValid("http://www.google.com//"));
+//        assertTrue(validator.isValid("https://www.google.com")); ---------------------EXCEPTION
+//        assertFalse(validator.isValid("htttps://www.google.com"));--------------------EXCEPTION
+        assertTrue(validator.isValid("http://www.google.org/"));
+        assertFalse(validator.isValid("http//www.google.org/"));
+//        assertTrue(validator.isValid("http://www.google.org:8080"));  ----------------FAILING
+//        assertTrue(validator.isValid("http://www.google.com:65535/t123")); -----------FAILING
+//        assertFalse(validator.isValid("http://www.google.org:118080"));
+//        assertTrue(validator.isValid("ftp://osu.edu")); ------------------------------EXCEPTION
+        assertTrue(validator.isValid("http://qwerty.ua/asdf?param=test"));
+        assertTrue(validator.isValid("http://qwerty.ua/zxcv?param=test2&another-param=123456"));
+        assertTrue(validator.isValid("http://http.xyz/"));
+        assertTrue(validator.isValid("http://com.http"));
+    }
 
 
+    public void testYourFirstPartition() {
+        //You can use this function to implement your First Partition testing
+    }
+
+    public void testYourSecondPartition() {
+        //You can use this function to implement your Second Partition testing
+    }
+    //You need to create more test cases for your Partitions if you need to
+
+    public void testIsValid() {
+        //You can use this function for programming based testing
+    }
 }
