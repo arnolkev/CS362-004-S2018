2968e3ddb68e057e4938c2daad33571b0036d2ef
arnolkev <arnolkev@oregonstate.edu>
2018-05-29 12:51:33 -0700

    Update UrlValidatorTest.java

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -27,15 +27,96 @@ public class UrlValidatorTest extends TestCase {
 
 
     public void testYourFirstPartition() {
-        //You can use this function to implement your First Partition testing
+        UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        //testing valid and invalid URL Schemes
+        String[] goodSchemes = {"", "h://", "http://", "f5p://", "f-.+p://", "ftttttttttp://"}
+        String[] badSchemes = {"://", "3ttp://", "f$p://" "h:", "h//", "h:/"}
+        int i;
+        for (i = 0; i < goodSchemes.length; i++){
+            StringBuilder tester = goodSchemes[i];
+            tester.append("www.google.com");
+            assertTrue(validator.isValid(tester));
+        }
+        for (i = 0; i < badSchemes.length; i++){
+            StringBuilder tester = badSchemes[i];
+            tester.append("www.google.com");
+            assertFalse(validator.isValid(tester));
+        }
+
+
     }
 
     public void testYourSecondPartition() {
-        //You can use this function to implement your Second Partition testing
-    }
-    //You need to create more test cases for your Partitions if you need to
+        UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+         //testing valid and invalid URL authoirities
+        String[] goodAuth = {"www.google.com", "w.google.com", "google.com", "g00gle.com", "g.com", "google.ca", "0.0.0.0", "1.1.1.1", "255.255.255.255" }
+        String[] badAuth = { "", "www..com", ".com", "google.", "g$$gle.com", "256.256.256.256", "0.0.0", "0.0.0.0.0", ".0.0.0.0", "0.0.0.0.", "-1.-1.-1.-1" }
+        int i;
+        for (i = 0; i < goodAuth.length; i++){
+            StringBuilder tester = "http://";
+            tester.append(goodAuth[i]);
+            assertTrue(validator.isValid(tester));
+        }
+        for (i = 0; i < badAuth.length; i++){
+            StringBuilder tester = "http://";
+            tester.append(badAuth[i]);
+            assertFalse(validator.isValid(tester));
+        }
 
+    }
+    public void testYourThirdPartition() {
+        UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+              //testing valid and invalid URL ports
+        String[] goodPort = {"", ":0", ":1", ":80", ":65535" }
+        String[] badPort = {":65536", ":-1", ":1a", ":a" }
+        int i;
+        for (i = 0; i < goodPort.length; i++){
+            StringBuilder tester = "http://www.google.com";
+            tester.append(goodPort[i]);
+            assertTrue(validator.isValid(tester));
+        }
+        for (i = 0; i < badPort.length; i++){
+            StringBuilder tester = "http://www.google.com";
+            tester.append(badPort[i]);
+            assertFalse(validator.isValid(tester));
+        }
+    }
+    public void testYourFourthPartition() {
+        UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+              //testing valid and invalid URL paths
+        String[] goodPath = {"", "/valid","/v1%", "/111", "/&&&", "/this/is/vaild", "/stillvaild/"}
+        String[] badPath = {"//invalid", "/this//alsoinvalid", "/not:good/", }
+        int i;
+        for (i = 0; i < goodPath.length; i++){
+            StringBuilder tester = "http://www.google.com";
+            tester.append(goodPath[i]);
+            assertTrue(validator.isValid(tester));
+        }
+        for (i = 0; i < badPath.length; i++){
+            StringBuilder tester = "http://www.google.com";
+            tester.append(badPath[i]);
+            assertFalse(validator.isValid(tester));
+        }
+    }
+    public void testYourFifthPartition() {
+        UrlValidator validator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+               //testing valid and invalid URL query
+        String[] goodQ = { "", "?this=good", "?this=good&sois=this", "?this=good;sois=this", "?works=this+thing+works" }
+        String[] badQ = { "?"}
+        int i;
+        for (i = 0; i < goodQ.length; i++){
+            StringBuilder tester = "http://www.google.com";
+            tester.append(goodQ[i]);
+            assertTrue(validator.isValid(tester));
+        }
+        for (i = 0; i < badQ.length; i++){
+            StringBuilder tester = "http://www.google.com";
+            tester.append(badQ[i]);
+            assertFalse(validator.isValid(tester));
+        }
+    }
     public void testIsValid() {
         //You can use this function for programming based testing
     }
 }
+
